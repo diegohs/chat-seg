8a9e9f5001e6e4645fed7f2e689a184316d1fb0b
diego.henrique <diego.henrique@D102510>
2015-05-28 20:30:43 -0300

    Configurada a negociação de chaves usando Diffie Hellman

--- Chat/src/chat/Chat.java
+++ Chat/src/chat/Chat.java
@@ -9,6 +9,7 @@ import java.net.Socket;
 import java.nio.charset.Charset;
 
 import crypto.Crypto;
+import crypto.DiffieHellman;
 import crypto.Rc4;
 import crypto.Sdes;
 
@@ -17,7 +18,10 @@ public class Chat extends Thread {
 	private static Socket conexao;
 	private static String key = "";
 	private static Crypto crypto;
-	private static String alg;
+	private static String alg = "rc4";
+	private static DiffieHellman dh;
+	private static String tipo;
+	private static boolean negocia = true;
 	
 	public static Crypto getCrypto() {
 		return crypto;
@@ -46,6 +50,7 @@ public class Chat extends Thread {
     public static void main(String args[])
     {
     	Socket socket = null;
+    	dh = new DiffieHellman();
     	
     	if (args.length != 4){
     		System.out.println("Uso: chat <cliente|servidor> <endereco> <porta> <rc4|sdes>");
@@ -59,12 +64,14 @@ public class Chat extends Thread {
 	    		if(args[0].equalsIgnoreCase("cliente")){
 	    			String endereco = args[1];
 	            	int porta = Integer.valueOf(args[2]);
+	            	tipo = "cliente";
 	            	
 	            	// Conecta ao servidor
             		socket = new Socket(endereco,porta);
 	    			
 	    		} else if(args[0].equalsIgnoreCase("servidor")){
 	    			int porta = Integer.valueOf(args[2]);
+	    			tipo = "servidor";
 	    			
 	    			// Abre um socket na porta especificada
 					ServerSocket server = new ServerSocket(porta);
@@ -79,7 +86,7 @@ public class Chat extends Thread {
                 Thread thread = new Chat(socket);
                 thread.start();
                 
-                // Inicia criptografia
+                /*// Inicia criptografia
                 if(alg.equalsIgnoreCase("rc4")){
         			// Criptografia RC4
                 	setKey("1010101010");
@@ -89,16 +96,28 @@ public class Chat extends Thread {
         			setKey("1010101010");
         	    	crypto = new Sdes(getKey());
         		} else {
-        			System.out.println(": \tchat <cliente> <endereco> <porta>");
-            		System.out.println("\tchat <servidor> <porta>");
+        			System.out.println("Uso: chat <cliente|servidor> <endereco> <porta> <rc4|sdes>");
             		System.exit(-1);
-        		}
+        		}*/
                 
                 // Processo para leitura do teclado e envio de mensagens
                 DataOutputStream  saida = new DataOutputStream(socket.getOutputStream()); 
                 BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in,Charset.forName("UTF-8")));
                 String msg;
                 byte[] msgCriptografada;
+                
+                // Negocia chaves de sessão
+                if(tipo.equalsIgnoreCase("cliente")){
+	                msgCriptografada = String.valueOf(dh.getPubY()).getBytes();
+	                try{
+	                	saida.writeInt(msgCriptografada.length);
+	                    saida.write(msgCriptografada);
+	                }
+	                catch(Exception e){
+	                    System.err.println(e.getMessage());
+	                }
+                }
+                
                 
                 // Leitura das mensagens digitadas para enviar
                 while (true)
@@ -135,8 +154,48 @@ public class Chat extends Thread {
     	int length;
         try {
         	DataInputStream  entrada = new DataInputStream(conexao.getInputStream());
-        	String msg;
+        	String msg = null;
             byte[] msgCriptografada = null;
+            
+            // Negocia chaves
+            while (negocia && tipo.equalsIgnoreCase("servidor"))
+            {
+            	length = entrada.readInt();
+            	if(length>0) {
+            	    byte[] message = new byte[length];
+            	    entrada.readFully(message, 0, message.length);
+            	    msg = new String(message,Charset.forName("UTF-8"));
+            	}
+                String chave = dh.calcK(Integer.valueOf(msg));
+                setCrypto(alg, chave);
+                
+                // Envia chave publica para cliente
+                DataOutputStream  saida = new DataOutputStream(conexao.getOutputStream());
+                msgCriptografada = String.valueOf(dh.getPubY()).getBytes();
+                try{
+                	saida.writeInt(msgCriptografada.length);
+                    saida.write(msgCriptografada);
+                }
+                catch(Exception e){
+                    System.err.println(e.getMessage());
+                }                
+                
+                negocia = false;
+            }
+            
+            // Cliente recebe a chave publica e configura criptografia
+            while (negocia && tipo.equalsIgnoreCase("cliente"))
+            {
+            	length = entrada.readInt();
+            	if(length>0) {
+            	    byte[] message = new byte[length];
+            	    entrada.readFully(message, 0, message.length);
+            	    msg = new String(message,Charset.forName("UTF-8"));
+            	}
+                String chave = dh.calcK(Integer.valueOf(msg));
+                setCrypto(alg, chave);
+                negocia = false;
+            }
             
             // Leitura das mensagens recebidas
             while (true)
